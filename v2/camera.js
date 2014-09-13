9fe769ec60b582967fc4c9b930780463faf6719c
Jeiel Aranal <jeiel.aranal@gmail.com>
2014-09-14 04:27:35 +0800

    Working with one palette and only nearest color mode.

--- v2/camera.js
+++ v2/camera.js
@@ -1,6 +1,7 @@
 var $GzCam = {
 	size: 256,
 	sizeH: 256/2,
+	defaults: {x:0, y:6},
 	webGl: function() {
 		return ( function () { try { return !! window.WebGLRenderingContext && !! document.createElement( 'canvas' ).getContext( 'experimental-webgl' ); } catch( e ) { return false; } } )();
 	},
@@ -19,7 +20,7 @@ var $GzCam = {
 
 		// Setup renderer
 		if ( $GzCam.webGl() )
-			$GzCam.renderer = new THREE.WebGLRenderer( {antialias:true} );
+			$GzCam.renderer = new THREE.WebGLRenderer( {antialias:true, preserveDrawingBuffer: true} );
 		else
 			$GzCam.renderer = new THREE.CanvasRenderer();
 		$GzCam.renderer.setSize($GzCam.size, $GzCam.size);
@@ -31,17 +32,28 @@ var $GzCam = {
 		$GzCam.videoTexture.magFilter = THREE.NearestFilter;
 
 		// Setup material material
+		$GzCam.uniforms = {
+			baseTexture: { type: 't', value: $GzCam.videoTexture },
+			ditherTexture: { type: 't', value: THREE.ImageUtils.loadTexture('images/packedBayerMaps.png') },
+			resolution: { type: "v2", value: new THREE.Vector2(8, 8) },
+			pal1: { type: "v3", value: new THREE.Vector3(0.10,0.25,0.17) },
+			pal2: { type: "v3", value: new THREE.Vector3(0.30,0.47,0.20) },
+			pal3: { type: "v3", value: new THREE.Vector3(0.66,0.74,0.23) },
+			pal4: { type: "v3", value: new THREE.Vector3(0.82,0.88,0.56) },
+			setting: { type: "v2", value: new THREE.Vector2($GzCam.defaults.x, $GzCam.defaults.y) }
+		};
+		$GzCam.uniforms.ditherTexture.minFilter = THREE.NearestFilter;
+		$GzCam.uniforms.ditherTexture.magFilter = THREE.NearestFilter;
+
 		$GzCam.material = new THREE.ShaderMaterial({
-			uniforms: {
-				baseTexture : { type: 't', value: $GzCam.videoTexture }
-			},
+			uniforms: $GzCam.uniforms,
 			vertexShader: document.querySelector('#vtx').innerHTML,
 			fragmentShader: document.querySelector('#frg').innerHTML
 		});
 
 		var geometry = new THREE.PlaneGeometry($GzCam.size, $GzCam.size);
 		$GzCam.mesh = new THREE.Mesh(geometry, $GzCam.material);
-		$GzCam.mesh.position.set(0,0,0);
+		$GzCam.mesh.position.set(0.5,0.5,0);
 		$GzCam.scene.add($GzCam.mesh);
 
 		$GzCam.render();
@@ -52,5 +64,16 @@ var $GzCam = {
 		if ($VidStream.copyToCanvas($GzCam.context))
 			$GzCam.videoTexture.needsUpdate = true;
 		$GzCam.renderer.render($GzCam.scene, $GzCam.camera);
+	},
+	takeSnap: function(){
+		var source = $GzCam.renderer.domElement.toDataURL("image/png");
+		var filmroll = document.getElementById("filmroll");
+		img = document.createElement("img");
+		img.src = source;
+		img.height = 0;
+		img.width = img.height = $GzCam.size;
+		img.style.top = -$GzCam.size;
+		filmroll.insertBefore(img, filmroll.firstChild);
+		morpheus(img, { top: 0, duration: 1000 });
 	}
 }
--- v2/ui-cam.js
+++ v2/ui-cam.js
@@ -1,7 +1,16 @@
 $CamUI = {
-	handedness: "left",
+	dpad: null,
+	options: null,
+	startVals: {},
 	init : function(){
+		$CamUI.dpad = document.querySelector('#dpad-settings');
+		$CamUI.options = document.querySelector('#options');
+		
+		$CamUI.saveStart($CamUI.dpad, 'top');
+		$CamUI.saveStart($CamUI.options, 'left');
+
 		document.querySelector('#toggle-source').addEventListener('click', $VidStream.toggleStream, false);
+
 		document.querySelector('#toggle-fullscreen').addEventListener('click', function(){
 			if (screenfull.enabled) {
 				if (screenfull.isFullscreen)
@@ -10,17 +19,36 @@ $CamUI = {
 					screenfull.request();
 			}
 		})
-		document.querySelector('#toggle-hand').addEventListener('click', $CamUI.toggleHand);
+
+		var settingsControl = document.querySelector('#toggle-settings');
+		settingsControl.addEventListener('click', function(){
+            morpheus($CamUI.dpad, { top: '-130px', duration: 250});
+            morpheus($CamUI.options, { left: '30px', duration: 250 });
+		});
+
+		var contrastControl = document.querySelector('#contrast')
+		contrastControl.value = $GzCam.defaults.x;
+		contrastControl.addEventListener('input', function(evt){
+			$GzCam.uniforms.setting.value.x = (evt.target.value/5) * 0.5;
+		});
+
+		var ditherControl = document.querySelector('#threshold');
+		ditherControl.value = $GzCam.defaults.y;
+		ditherControl.addEventListener('input', function(evt){
+			$GzCam.uniforms.setting.value.y = evt.target.value;
+		});
+
+		document.querySelector('#leave-options').addEventListener('click', function(){
+
+            morpheus($CamUI.dpad, { top: $CamUI.startVals[$CamUI.dpad.id], duration: 250 });
+            morpheus($CamUI.options, { left: $CamUI.startVals[$CamUI.options.id], duration: 250 });
+		});
+
+		document.querySelector('#snap').addEventListener('click', $GzCam.takeSnap);
 	},
-	toggleHand : function(){
-		if (document.querySelector('body#hand-right') == null)
-		{
-			document.body.id = "hand-right";
-		}
-		else
-		{
-			document.body.id = "hand-left";
-		}
+	saveStart: function(element, styleName){
+		if ($CamUI.startVals[element.id] === undefined)
+			$CamUI.startVals[element.id] = window.getComputedStyle(element)[styleName];
 	}
 }
 
