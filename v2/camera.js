92253a15ca526b885dd837b5d377fb1e18e27d4b
Jeiel Aranal <jeiel.aranal@gmail.com>
2014-09-15 01:36:20 +0800

    Added moar graphics, palette switching

--- v2/camera.js
+++ v2/camera.js
@@ -2,6 +2,27 @@ var $GzCam = {
 	size: 256,
 	sizeH: 256/2,
 	defaults: {x:0, y:6},
+	currentPal: 0,
+	palettes: [
+		[
+		[49, 74, 99],
+		[255, 99, 41],
+		[115,198,198],
+		[255, 214, 156]
+		],
+		[
+		[8, 56, 8],
+		[48, 96, 48],
+		[136,168,8],
+		[183, 220, 17]
+		],
+		[
+		[41, 41, 156],
+		[123, 49, 239],
+		[239, 140, 140],
+		[255, 198, 255]
+		]
+	],
 	webGl: function() {
 		return ( function () { try { return !! window.WebGLRenderingContext && !! document.createElement( 'canvas' ).getContext( 'experimental-webgl' ); } catch( e ) { return false; } } )();
 	},
@@ -34,12 +55,13 @@ var $GzCam = {
 		// Setup material settings
 		$GzCam.uniforms = {
 			baseTexture: { type: 't', value: $GzCam.videoTexture },
-			pal1: { type: "v3", value: new THREE.Vector3(0.10,0.25,0.17) },
-			pal2: { type: "v3", value: new THREE.Vector3(0.30,0.47,0.20) },
-			pal3: { type: "v3", value: new THREE.Vector3(0.66,0.74,0.23) },
-			pal4: { type: "v3", value: new THREE.Vector3(0.82,0.88,0.56) },
+			// pal1: { type: "v3", value: new THREE.Vector3(0.10,0.25,0.17) },
+			// pal2: { type: "v3", value: new THREE.Vector3(0.30,0.47,0.20) },
+			// pal3: { type: "v3", value: new THREE.Vector3(0.66,0.74,0.23) },
+			// pal4: { type: "v3", value: new THREE.Vector3(0.82,0.88,0.56) },
 			setting: { type: "v2", value: new THREE.Vector2($GzCam.defaults.x, $GzCam.defaults.y) }
 		};
+		$GzCam.paletteToUniform(1);
 
 		if (window.devicePixelRatio == 2) {
 			$GzCam.uniforms.ditherTexture = { type: 't', value: THREE.ImageUtils.loadTexture('images/packedBayerX4.png') };
@@ -85,5 +107,26 @@ var $GzCam = {
 		img.style.top = -$GzCam.size;
 		filmroll.insertBefore(img, filmroll.firstChild);
 		morpheus(img, { top: 0, duration: 1000 });
+	},
+	togglePalette: function(){
+		$GzCam.currentPal++;
+		if ($GzCam.currentPal >= $GzCam.palettes.length)
+			$GzCam.currentPal = 0;
+		$GzCam.paletteToUniform($GzCam.currentPal);
+	},
+	paletteToUniform: function(index){
+		$GzCam.currentPal = index;
+		for (var i = 0; i < $GzCam.palettes[index].length; i++)
+		{
+			var color = $GzCam.palettes[index][i];
+			var uniformName = 'pal' + (i+1);
+
+			if ($GzCam.uniforms[uniformName] === undefined)
+				$GzCam.uniforms[uniformName] = { type: 'v3' };
+
+			$GzCam.uniforms[uniformName].value = new THREE.Vector3(color[0]/255,
+																	color[1]/255,
+																	color[2]/255);
+		}
 	}
 }
